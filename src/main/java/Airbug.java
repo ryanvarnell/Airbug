c07b4b85e70e75606e1fe8200abcad2d2cb48840
Ryan Varnell <ryanv1022@gmail.com>
2021-11-26 21:14:51 -0500

    BIG CLEANUP

--- src/main/java/Airbug.java
+++ src/main/java/Airbug.java
@@ -13,48 +13,12 @@ public class Airbug {
             return gateway.on(MessageCreateEvent.class, event -> {
                 Message message = event.getMessage();
                 if (message.getContent().charAt(0) == '-') {
-                    return handleCommand(message);
+                    new CommandHandler(message);
+                    return CommandHandler.process();
                 }
                 return Mono.empty();
             });
         });
         login.block();
-    }
-
-    public static Mono<Message> handleCommand(Message message) {
-        String[] commandTokens = message.getContent().split("\\s+");
-        String rootCommand = commandTokens[0].substring(1);
-
-        switch (rootCommand) {
-            case "ping" -> {
-                return ping(message);
-            }
-            case "img" -> {
-                return getImage(message, commandTokens[1]);
-            }
-            case "bing" -> {
-                if (commandTokens[1].equalsIgnoreCase("chilling"))
-                    return bingChilling(message);
-                else
-                    return message.getChannel().flatMap(channel -> channel.createMessage("bong"));
-            }
-        }
-
-        return Mono.empty();
-    }
-
-    public static Mono<Message> ping(Message message) {
-        return message.getChannel().flatMap(channel -> channel.createMessage("pong!"));
-    }
-
-    public static Mono<Message> getImage(Message message, String searchTerm) {
-        return null;
-    }
-
-    public static void bing(Message message) {
-    }
-
-    public static Mono<Message> bingChilling(Message message) {
-        return message.getChannel().flatMap(channel -> channel.createMessage("<:cold_face:913836443695198231>"));
     }
 }
---
+++ src/main/java/CommandHandler.java
@@ -0,0 +1,40 @@
+import discord4j.core.object.entity.Message;
+import reactor.core.publisher.Mono;
+
+import java.util.Arrays;
+
+class CommandHandler {
+    static String rootCommand;
+    static String[] modifiers;
+    static Message message;
+
+    CommandHandler(Message message) {
+        CommandHandler.message = message;
+        parseCommand(message);
+    }
+
+    private void parseCommand(Message message) {
+        String[] tokenizedString = message.getContent().split("\\s+");
+        rootCommand = tokenizedString[0].substring(1);
+        modifiers = Arrays.copyOfRange(tokenizedString, 1, tokenizedString.length);
+    }
+
+    public static Mono<Message> process() {
+        switch (rootCommand) {
+            case "ping" -> { return ping(); }
+            case "bing" -> {
+                if (modifiers[0].equalsIgnoreCase("chilling"))
+                    return bingChilling();
+            }
+        }
+        return null;
+    }
+
+    private static Mono<Message> ping() {
+        return message.getChannel().flatMap(channel -> channel.createMessage("pong!"));
+    }
+
+    private static Mono<Message> bingChilling() {
+        return message.getChannel().flatMap(channel -> channel.createMessage(":cold_face:"));
+    }
+}
