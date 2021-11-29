b6a1b696ba2a17e65d4ac7caa9cd7fb542feaca2
Ryan Varnell <ryanv1022@gmail.com>
2021-11-29 16:51:12 -0500

    dont know what i did

--- src/main/java/Airbug.java
+++ src/main/java/Airbug.java
@@ -19,7 +19,6 @@ public class Airbug {
      * Main loop
      */
     public static void main(String[] args) {
-
         // Check messages for command prompt
         Mono<Void> login = client.withGateway((GatewayDiscordClient gateway) ->
                 gateway.on(MessageCreateEvent.class, event -> {
@@ -27,9 +26,7 @@ public class Airbug {
                     // If the user's message begins with the command prompt, open a new CommandHandler and send it the
                     // message to be processed.
                     if (message.getContent().startsWith(commandPrompt)) {
-                        Mono<Message> response = CommandHandler.process(message);
-                        message.delete();
-                        return response;
+                        return CommandHandler.process(message);
                     }
                     return Mono.empty();
                 }));
